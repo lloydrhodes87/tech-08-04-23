726a092d2a81e323d5bdcc43e7d16f15735b15c6
lloydrhodescinch87 <lloyd.rhodes@cinch.co.uk>
2023-04-08 09:02:13 +0100

    add step function definition

---
+++ cdk/constructs/createStepFunction.ts
@@ -0,0 +1,88 @@
+import {
+	aws_lambda,
+	aws_logs,
+	aws_stepfunctions,
+	aws_stepfunctions_tasks,
+	Duration,
+	RemovalPolicy,
+} from 'aws-cdk-lib';
+import { StateMachineType } from 'aws-cdk-lib/aws-stepfunctions';
+import { LambdaInvoke } from 'aws-cdk-lib/aws-stepfunctions-tasks';
+import { Construct } from 'constructs';
+import { getResourceName } from '../helpers/getResourceName';
+import { KrakenStackProps } from '../kraken-stack-props';
+
+export const createStepFunction = (
+	scope: Construct,
+	props: KrakenStackProps,
+	postOutageHandler: aws_lambda.Function,
+	dlqHandler: aws_lambda.Function,
+) => {
+	const stateMachineLogGroup = new aws_logs.LogGroup(
+		scope,
+		`${props.componentId}/StateMachineLogGroup`,
+		{
+			logGroupName: `/aws/vendedlogs/states/-${props.componentId}-state-machine`,
+			retention: aws_logs.RetentionDays.ONE_DAY,
+			removalPolicy: RemovalPolicy.DESTROY,
+		},
+	);
+
+	const stateMachine = new aws_stepfunctions.StateMachine(
+		scope,
+		'StateMachine',
+		{
+			stateMachineName: getResourceName(props, 'state-machine'),
+			definition: createStepFunctionDefinition(
+				scope,
+				postOutageHandler,
+				dlqHandler,
+			),
+			timeout: Duration.minutes(5),
+			stateMachineType: StateMachineType.EXPRESS,
+			logs: {
+				destination: stateMachineLogGroup,
+				level: aws_stepfunctions.LogLevel.ALL,
+				includeExecutionData: true,
+			},
+		},
+	);
+	return stateMachine;
+};
+
+const createStepFunctionDefinition = (
+	scope: Construct,
+	postOutageHandler: aws_lambda.Function,
+	dlqHandler: aws_lambda.Function,
+) => {
+	const postOutagesTask = new aws_stepfunctions_tasks.LambdaInvoke(
+		scope,
+		'Post outage',
+		{
+			lambdaFunction: postOutageHandler,
+			inputPath: '$',
+			outputPath: '$.Payload',
+		},
+	);
+
+	const sendErrorToDlq = new LambdaInvoke(scope, 'Handle DQL', {
+		lambdaFunction: dlqHandler,
+		inputPath: '$.Cause',
+		resultPath: '$',
+	});
+
+	const errorTask = new aws_stepfunctions.Pass(scope, 'errorTask', {
+		resultPath: '$',
+	});
+
+	const errorDefinition = sendErrorToDlq.next(errorTask);
+
+	return postOutagesTask
+		.addRetry({
+			errors: ['States.ALL'],
+			interval: Duration.seconds(1),
+			maxAttempts: 1,
+			backoffRate: 2,
+		})
+		.addCatch(errorDefinition);
+};
