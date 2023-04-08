9da6ffa81a3676f1c7fb8406ce3e6393fb5c9fe8
lloydrhodescinch87 <lloyd.rhodes@cinch.co.uk>
2023-04-08 11:33:25 +0100

    fix handler

--- src/handlers/postOutage/postOutageHandler.ts
+++ src/handlers/postOutage/postOutageHandler.ts
@@ -19,26 +19,26 @@ export const handler: Handler<OutagesRequest, OutagesResponse | void> = async (
 			callback(
 				new Error(`An error was forced for site: ${input.siteId}`),
 			);
-
-			const apiKey = await loadParam('kraken-api-key');
-
-			const outages = await getOutages(apiKey);
-			const sitesById = await getSiteById(input.siteId, apiKey);
-			const filteredOutages = filterOutages(outages.data, sitesById.data);
-
-			const namedOutages = attachOutageDisplayName(
-				filteredOutages,
-				sitesById.data.devices,
-			);
-
-			await postOutages(input.siteId, apiKey, namedOutages);
-
-			return {
-				statusCode: 200,
-				message: 'The following outages have been submitted',
-				outages: namedOutages,
-			};
 		}
+
+		const apiKey = await loadParam('kraken-api-key');
+
+		const outages = await getOutages(apiKey);
+		const sitesById = await getSiteById(input.siteId, apiKey);
+		const filteredOutages = filterOutages(outages.data, sitesById.data);
+
+		const namedOutages = attachOutageDisplayName(
+			filteredOutages,
+			sitesById.data.devices,
+		);
+
+		await postOutages(input.siteId, apiKey, namedOutages);
+
+		return {
+			statusCode: 200,
+			message: 'The following outages have been submitted',
+			outages: namedOutages,
+		};
 	} catch (error) {
 		return callback(new Error(`Error: ${error}`));
 	}
